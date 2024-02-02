be28d6505f0d8dcba9db4b12778c3292b2f0d9d8
Lou Batier <lou.batier@linkfluence.com>
2024-02-02 01:14:45 +0100

    feat: wowaudit proxy

--- components/reclear-table/index.tsx
+++ components/reclear-table/index.tsx
@@ -1,26 +1,13 @@
-// Import necessary modules
 import { useQuery } from "react-query";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import groupBy from "lodash/groupBy";
 import {
-  Character,
-  ECharacterRole,
-  EClassColor,
   ESignupStatus,
   Encounter,
   Roster,
   Signup,
 } from "../../data/models/roster";
 import styled from "styled-components";
-import {
-  ArrowRight,
-  ArrowRightCircle,
-  ArrowRightFromLine,
-  ArrowRightToLine,
-  Copy,
-} from "lucide-react";
-import { captureComponent } from "../../lib/screenshot";
 import {
   getRosterKeyFromCharacterRole,
   replaceWhitespaceWithUnderscore,
@@ -66,12 +53,7 @@ const ReclearTable: React.FC<IProps> = ({ raid }) => {
     isLoading: boolean;
   } = useQuery(
     ["raidData", raid],
-    () =>
-      axios
-        .get(
-          `https://wowaudit.com/v1/raids/${raid}?api_key=43b8bea3357ac3a65a9a75ccd363ec85b722006e6efeaf92f6c1f9e38d8cab30`
-        )
-        .then((res) => res.data),
+    () => axios.get(`/api/wowaudit-proxy?raid=${raid}`).then((res) => res.data),
     {
       enabled: !!raid,
       retry: false,
---
+++ pages/api/wowaudit-proxy.js
@@ -0,0 +1,17 @@
+export default async function handler(req, res) {
+  const raid = req.query.raid;
+  const apiKey =
+    "43b8bea3357ac3a65a9a75ccd363ec85b722006e6efeaf92f6c1f9e38d8cab30"; // Consider moving this to environment variables
+
+  const url = `https://wowaudit.com/v1/raids/${raid}?api_key=${apiKey}`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error(`Failed to fetch data from WoW Audit`);
+
+    const data = await response.json();
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
