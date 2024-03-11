602d9b78719c6120496835eaef35f55a999783d0
unknown <lou@sleede.com>
2024-03-11 19:35:49 +0100

    fix: changed body color to red

--- components/globalstyles.tsx
+++ components/globalstyles.tsx
@@ -3,7 +3,7 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
   html,
   body {
-    color: ${({ theme }) => theme.colors.primary};
+    color: red;
     background-color: ${({ theme }) => theme.colors.application_background};
     padding: 0;
     margin: 0;
