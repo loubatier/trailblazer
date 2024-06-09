066b0e8426e152f694f069d437800359d876833f
Lou Batier <lou.batier@meltwater.com>
2024-06-10 01:43:25 +0200

    fix: remove counter

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,21 +1,14 @@
 import React from "react";
 import Link from "next/link";
-import Counter from "../components/counter";
 import { Description, Main, Title } from "../components/sharedstyles";
-import { useCounterStore } from "../lib/stores";
-import useStore from "../lib/stores/useStore";
 
 const About = () => {
-  const counterState = useStore(useCounterStore, (state) => state);
-
   return (
     <Main>
       <Title>About Page</Title>
       <Description>
         <Link href="/">&larr; Go Back</Link>
       </Description>
-      <div>COUNT: {counterState?.count}</div>
-      <Counter label="ABOUT" />
     </Main>
   );
 };
